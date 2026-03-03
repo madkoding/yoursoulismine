7c1d46daff764fa75f63159120d44b59b475bf51
madkoding <madkoding@gmail.com>
2026-03-03 10:23:29 -0300

    Fix Prisma config for Vercel with DATABASE_URL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,11 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 const databaseUrl = process.env.DATABASE_URL || './.vercel/sqlite.db'
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: databaseUrl
-    }
-  }
+  log: ['query', 'error', 'warn'],
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
