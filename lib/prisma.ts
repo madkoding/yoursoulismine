95f75d93ae7715130345e8efcbabdc717579da8c
madkoding <madkoding@gmail.com>
2026-03-03 10:22:49 -0300

    Configure Vercel with SQLite and DATABASE_URL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,14 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const databaseUrl = process.env.DATABASE_URL || './.vercel/sqlite.db'
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl
+    }
+  }
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
